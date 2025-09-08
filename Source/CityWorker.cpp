a90cd57789ce38514acb88bd6675c22854b814e6
EMJOE CITY <inclinedce@gmail.com>
2025-09-07 23:42:29 -0700

    Restore all source files from items

--- Source/CityWorker.cpp
+++ Source/CityWorker.cpp
@@ -1,11 +1,22 @@
-#include "CityWorker.h"
+﻿#include "CityWorker.h"
+#include "Job.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+#include "NavigationSystem.h"
 #include "TimerManager.h"
 #include "JobManager.h"
+#include "Engine/World.h"
 
 ACityWorker::ACityWorker()
 {
     PrimaryActorTick.bCanEverTick = false;
     JobManager = nullptr;
+    IdleLocation = FVector::ZeroVector;
+}
+
+void ACityWorker::BeginPlay()
+{
+    Super::BeginPlay();
+    IdleLocation = GetActorLocation();
 }
 
 void ACityWorker::SetJobManager(AJobManager* Manager)
@@ -13,8 +24,10 @@ void ACityWorker::SetJobManager(AJobManager* Manager)
     JobManager = Manager;
 }
 
-void ACityWorker::StartJob()
+void ACityWorker::StartJob(FVector JobLocation)
 {
+    // Start job logic here, e.g. move to JobLocation
+
     if (GetWorld())
     {
         GetWorld()->GetTimerManager().SetTimer(WorkTimerHandle, this, &ACityWorker::CompleteJob, 5.0f, false);
@@ -23,8 +36,28 @@ void ACityWorker::StartJob()
 
 void ACityWorker::CompleteJob()
 {
-    if (JobManager)
+    // Job completion logic here
+}
+
+void ACityWorker::GoToIdleLocation()
+{
+    UNavigationSystemV1* NavSys = UNavigationSystemV1::GetCurrent(GetWorld());
+    if (NavSys)
     {
-        JobManager->OnJobCompleted(this);
+        AController* MyController = GetController();
+        if (MyController)
+        {
+            UAIBlueprintHelperLibrary::SimpleMoveToLocation(MyController, IdleLocation);
+        }
     }
 }
+
+void ACityWorker::EquipTool(AToolBase* Tool)
+{
+    this->EquippedTool = Tool;
+    if (Tool)
+    {
+        Tool->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, TEXT("ToolSocket"));
+    }
+}
+
